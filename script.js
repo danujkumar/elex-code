54a8c2599020a970e071accdbcdff9c27e17fbb7
D Anuj Kumar <92854468+danujkumar@users.noreply.github.com>
2022-11-03 13:15:27 +0530

    Second Commit

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function darkmode() {
     document.getElementById("header").style.color="white";
     document.getElementById("content").style.backgroundColor="#353839";
     document.getElementById("content").style.color="white";
+    document.getElementById("h1").style.color="white";
+    document.getElementById("h2").style.color="white";
+    document.getElementById("h3").style.color="white";
+    document.getElementById("h4").style.color="white";
+    document.getElementById("h5").style.color="white";
+    document.getElementById("h6").style.color="white";
+    document.getElementById("h7").style.color="white";
     document.getElementById("dark-btn").style.display="none";
     document.getElementById("light-btn").style.display="block";
     console.log("dark");
@@ -14,6 +21,13 @@ function lightmode() {
     document.getElementById("top").style.backgroundColor="#c68642";
     document.getElementById("header").style.backgroundColor="#c68642";
     document.getElementById("content").style.backgroundColor="#ffdbac";
+    document.getElementById("h1").style.color="rgb(22, 22, 22)";
+    document.getElementById("h2").style.color="rgb(37, 37, 37)";
+    document.getElementById("h3").style.color="rgb(76, 76, 76)";
+    document.getElementById("h4").style.color="rgb(76, 76, 76)";
+    document.getElementById("h5").style.color="rgb(76, 76, 76)";
+    document.getElementById("h6").style.color="rgb(76, 76, 76)";
+    document.getElementById("h7").style.color="rgb(76, 76, 76)";
     document.getElementById("dark-btn").style.display="block";
     document.getElementById("light-btn").style.display="none";
     console.log("light");
--- second-floor.js
+++ second-floor.js
@@ -70,7 +70,6 @@ let path4 = document.getElementById("path4");
 let path5 = document.getElementById("path5");
 let path6 = document.getElementById("path6");
 let path7 = document.getElementById("path7");
-// let path8 = document.getElementById("path8");
 let path9 = document.getElementById("path9");
 let path10 = document.getElementById("path10");
 let path11 = document.getElementById("path11");
@@ -95,16 +94,26 @@ function removeAlll() {
     }
 }
 
+function removeDestinationAll() {
+  for(let i=1;i<=20;i++)
+  {
+    if(starting!=i)
+      document.getElementById(i.toString()).setAttribute("fill","#B6AEA1");
+  }
+}
+
 const getsetGo = (room_id) => {
     removeAlll();
   let getsetRoom = document.getElementById(room_id);
   if (starting == -1) {
     getsetRoom.setAttribute("fill", "#1EF53E");
     starting = Number.parseInt(getsetRoom.id);
-  } else if (starting != -1 && ending == -1) {
+  } else if (starting != -1 && ending == -1 && starting != Number.parseInt(room_id)) {
     getsetRoom.setAttribute("fill", "#F70808");
     ending = Number.parseInt(getsetRoom.id);
-  } else if (starting != -1 && ending != -1) {
+    locates();
+  } else if (starting != -1 && ending != -1 && (starting != Number.parseInt(room_id))) {
+    removeDestinationAll();
     getsetRoom.setAttribute("fill", "#F70808");
     ending = Number.parseInt(getsetRoom.id);
     locates();
@@ -228,79 +237,90 @@ const locates = () => {
     path23.style.transform = `rotate(${degree+"deg"})`;
   }
 
-
-//   let outofpark = [1];
-//   let firstrow = [2, 3, 4, 5, 6, 7, 8];
-//   let firstrowup = [17, 18, 19, 20];
-//   let firstrowdown = [13, 14, 15, 16];
-//   let secondrow = [9, 10, 11, 12];
-//   let firstrowdownpara = [];
-
   if (outofpark.includes(starting)) {
-
+    path23Con(180);path20Con(180);
     if(firstrow.includes(ending))
     {
         switch (ending) {
             case 2:
-                path23Con(180);
-                path20Con(180);
-                path22Con(180);
                 path1Con(-90);
                 break;
             case 3:
+                path1Con(0);path2Con(-90);
                 break;
             case 4:
+                path1Con(0);path2Con(0);path3Con(-90);
                 break;
-
             case 5:
+                path1Con(0);path2Con(0);path3Con(0);path4Con(-90);
                 break;
             case 6:
-                path23Con(180);
-                path20Con(180);
-                path22Con(180);
                 path1Con(0);path2Con(0);path3Con(0);path4Con(0);path5Con(-90);
                 break;
             case 7: 
+                path1Con(0); path2Con(0); path3Con(0);path4Con(0);path5Con(0);path6Con(-90);
                 break;
             case 8: 
-                break;
-
-            default:
+            path1Con(0); path2Con(0); path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(-90);
                 break;
         }
     }
 
     else if(firstrowup.includes(ending))
     {
+        path1Con(0);path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
         switch(ending)
         {
+            case 17:
+              path10Con(-90);path9Con(180);path17Con(180);
+              break;
+            case 18:
+              path10Con(-90);path9Con(180);path17Con(-90);
+              break;
+            case 19:
+              path10Con(-90);path9Con(180);path17Con(-45);
+              break;
             case 20:
-                path23Con(180);
-                path20Con(180);
-                path22Con(180);
-                path1Con(0);
-                path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);path10Con(-90);path9Con(180);path17Con(0);
-                break;
+              path10Con(-90);path9Con(180);path17Con(0);
+              break;
         }
     }
     else if(firstrowdown.includes(ending))
     {
+        path1Con(0);path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
         switch (ending) {
+            case 13:
+              path10Con(45);
+              break;
+            case 14:
+              path10Con(90);path11Con(-90);
+              break;
+            case 15:
+              path10Con(90);path11Con(0);path18Con(-90);
+              break;
             case 16:
-                path23Con(180);
-                path20Con(180);
-                path22Con(180);
-                path1Con(0);
-                path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);path10Con(90);path11Con(0);path18Con(0);path19Con(-90);
-                break;
-        
-            default:
+                path10Con(90);path11Con(0);path18Con(0);path19Con(-90);
                 break;
         }
     }
     else if(secondrow.includes(ending))
     {
-
+      path1Con(0);path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
+        switch(ending)
+        {
+          case 9:
+            path10Con(-45);
+            break;
+          case 10:
+            path10Con(0);path12Con(0);path13Con(-90);
+            break;
+          case 11:
+            path10Con(0);path12Con(0);path13Con(-15);
+            break;
+          case 12:
+            path10Con(0);path12Con(0);path13Con(0);
+            break;
+        }
     }
     else if(firstrowdownpara.includes(ending))
     {
@@ -308,30 +328,127 @@ const locates = () => {
     }
     else
     {
-
+      
+      alert("Same destination, please choose a different destination.")
+      removeAlll();
     }
     
-  } else if (firstrow.includes(starting)) {
+  } else if (firstrow.includes(starting)) 
+  {
+
+    switch(starting)
+    {
+      case 2:
+        path1Con(90);path2Con(0);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
+        break;
+      case 3:
+        path2Con(90);path3Con(0);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
+        break;
+      case 4:
+        path3Con(90);path4Con(0);path5Con(0);path6Con(0);path7Con(0);
+        break;
+      case 5:
+        path4Con(90);path5Con(0);path6Con(0);path7Con(0);
+        break;
+      case 6:
+        path5Con(90);path6Con(0);path7Con(0);
+        break;
+      case 7:
+        path6Con(90);path7Con(0);
+        break;
+      case 8:
+        path7Con(0);
+        break;
+    }
+
 
     if(outofpark.includes(ending))
     {
-
+      removeAlll();
+      path20Con(0);path23Con(0);
+      switch(starting)
+      {
+        case 2:
+          path1Con(180);
+          break;
+        case 3:
+          path1Con(180);path2Con(90);
+          break;
+        case 4:
+          path1Con(180);path2Con(180);path3Con(90);
+          break;
+        case 5:
+          path1Con(180);path2Con(180);path3Con(180);path4Con(90);
+          break;
+        case 6:
+          path1Con(180);path2Con(180);path3Con(180);path4Con(180);path5Con(90);
+          break;
+        case 7:
+          path1Con(180);path2Con(180);path3Con(180);path4Con(180);path5Con(180);path6Con(90);
+          break;
+        case 8:
+          path1Con(180);path2Con(180);path3Con(180);path4Con(180);path5Con(180);path6Con(180);path7Con(90);
+          break;
+      }
     }
     else if(firstrowup.includes(ending))
     {
-
+      switch(ending)
+      {
+        case 17:
+          path10Con(-90);path9Con(180);path17Con(180);
+          break;
+        case 18:
+          path10Con(-90);path9Con(180);path17Con(-90);
+          break;
+        case 19:
+          path10Con(-90);path9Con(180);path17Con(-45);
+          break;
+        case 20:
+          path10Con(-90);path9Con(180);path17Con(0);
+          break;
+      }
+      
     }
     else if(firstrowdown.includes(ending))
     {
-
+      switch (ending) {
+        case 13:
+          path10Con(45);
+          break;
+        case 14:
+          path10Con(90);path11Con(-90);
+          break;
+        case 15:
+          path10Con(90);path11Con(0);path18Con(-90);
+          break;
+        case 16:
+            path10Con(90);path11Con(0);path18Con(0);path19Con(-90);
+            break;
+    }
     }
     else if(secondrow.includes(ending))
     {
-
+      switch(ending)
+        {
+          case 9:
+            path10Con(-45);
+            break;
+          case 10:
+            path10Con(0);path12Con(0);path13Con(-90);
+            break;
+          case 11:
+            path10Con(0);path12Con(0);path13Con(-15);
+            break;
+          case 12:
+            path10Con(0);path12Con(0);path13Con(0);
+            break;
+        }
     }
-    else if(firstrowdownpara.includes(ending))
+    else if(firstrow.includes(ending))
     {
-
+       alert("Destination is on the same row.");
+       removeAlll();
     }
     else
     {
@@ -339,8 +456,280 @@ const locates = () => {
     }
 
   } else if (firstrowup.includes(starting)) {
+
+    switch(starting){
+      case 17:
+        path9Con(0);
+        break;
+      case 18:
+        path17Con(90);path9Con(0);
+        break;
+      case 19:
+        path17Con(135);path9Con(0);    
+        break;
+      case 20:
+        path17Con(180);path9Con(0);
+        break;
+    }
+
+    if(firstrow.includes(ending) || outofpark.includes(ending))
+    {
+      switch(ending){
+        case 8:
+          path7Con(-90);path10Con(180);
+          break;
+        case 7:
+          path7Con(180);path6Con(-90);
+          break;
+        case 6:
+          path7Con(180);path6Con(180);path5Con(-90);
+          break;
+        case 5:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(-90);
+          break;
+        case 4:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(-90);
+          break;
+        case 3:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(-90);
+          break;
+        case 2:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(-90);
+          break;
+        case 1:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(180);path20Con(0);path23Con(0);
+          break;
+      }
+    }
+    else if(firstrowdown.includes(ending))
+    {
+      switch(ending)
+      {
+        case 13:
+          path10Con(45);
+          break;
+        case 14:
+          path10Con(90);path11Con(-90);
+          break;
+        case 15:
+          path10Con(90);path11Con(0);path18Con(-90);
+          break;
+        case 16:
+          path10Con(90);path11Con(0);path18Con(0);path19Con(-90);
+          break;
+      }
+    }
+    else if(secondrow.includes(ending))
+    {
+      switch(ending)
+        {
+          case 9:
+            path10Con(0);path12Con(-90);
+            break;
+          case 10:
+            path10Con(0);path12Con(0);path13Con(-90);
+            break;
+          case 11:
+            path10Con(0);path12Con(0);path13Con(-15);
+            break;
+          case 12:
+            path10Con(0);path12Con(0);path13Con(0);
+            break;
+        }
+    }
+    else
+    {
+      removeAlll();
+      if(ending == 17)
+        alert("Your destination is on the front.");
+      else
+        alert("Destination is on the same row.");
+    }
+
+
   } else if (firstrowdown.includes(starting)) {
+    switch(starting)
+    {
+      case 13:
+        path11Con(180);
+        break;
+      case 14:
+        path11Con(180);
+        break;
+      case 15:
+        path18Con(90);path11Con(180);
+        break;
+      case 16:
+        path19Con(90);path18Con(180);path11Con(180);
+        break;
+    }
+    
+    if(firstrow.includes(ending) || outofpark.includes(ending))
+    {
+      switch(ending){
+        case 8:
+          path7Con(-90);path10Con(180);
+          break;
+        case 7:
+          path7Con(180);path6Con(-90);path10Con(180);
+          break;
+        case 6:
+          path7Con(180);path6Con(180);path5Con(-90);path10Con(180);
+          break;
+        case 5:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(-90);path10Con(180);
+          break;
+        case 4:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(-90);path10Con(180);
+          break;
+        case 3:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(-90);path10Con(180);
+          break;
+        case 2:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(-90);path10Con(180);
+          break;
+        case 1:
+          path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(180);path20Con(0);path23Con(0);path10Con(180);
+          break;
+      }
+    }
+
+    else if(secondrow.includes(ending))
+    {
+      switch(ending)
+        {
+          case 9:
+            path10Con(0);path12Con(-90);
+            break;
+          case 10:
+            path10Con(0);path12Con(0);path13Con(-90);
+            break;
+          case 11:
+            path10Con(0);path12Con(0);path13Con(-15);
+            break;
+          case 12:
+            path10Con(0);path12Con(0);path13Con(0);
+            break;
+        }
+    }
+
+    else if(firstrowup.includes(ending))
+    {
+      switch(ending)
+        {
+            case 17:
+              path10Con(-90);path9Con(180);path17Con(180);
+              break;
+            case 18:
+              path10Con(-90);path9Con(180);path17Con(-90);
+              break;
+            case 19:
+              path10Con(-90);path9Con(180);path17Con(-45);
+              break;
+            case 20:
+              path10Con(-90);path9Con(180);path17Con(0);
+              break;
+        }
+    }
+    else
+    {
+      removeAlll();
+      alert("Destination is on the same row.");
+    }
+
+
   } else if (secondrow.includes(starting)) {
+
+    switch(starting)
+        {
+          case 12:
+            path12Con(180);path13Con(180);
+            break;
+          case 11:
+            path12Con(180);path13Con(165);
+            break;
+          case 10:
+            path12Con(180);path13Con(135);
+            break;
+          case 9:
+            path12Con(135);
+            break;
+        }
+    
+
+        if(firstrow.includes(ending) || outofpark.includes(ending))
+        {
+          switch(ending){
+            case 8:
+              path7Con(-90);path10Con(180);
+              break;
+            case 7:
+              path7Con(180);path6Con(-90);path10Con(180);
+              break;
+            case 6:
+              path7Con(180);path6Con(180);path5Con(-90);path10Con(180);
+              break;
+            case 5:
+              path7Con(180);path6Con(180);path5Con(180);path4Con(-90);path10Con(180);
+              break;
+            case 4:
+              path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(-90);path10Con(180);
+              break;
+            case 3:
+              path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(-90);path10Con(180);
+              break;
+            case 2:
+              path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(-90);path10Con(180);
+              break;
+            case 1:
+              path7Con(180);path6Con(180);path5Con(180);path4Con(180);path3Con(180);path2Con(180);path1Con(180);path20Con(0);path23Con(0);path10Con(180);
+              break;
+          }
+        }
+        else if(firstrowdown.includes(ending))
+        {
+          switch(ending)
+          {
+            case 13:
+              path10Con(90);break;
+            case 14:
+              path11Con(-90);path10Con(90);
+              break;
+            case 15:
+              path11Con(0);path18Con(-90);path10Con(90);
+              break;
+            case 16:
+              path11Con(0);path18Con(0);path19Con(-90);path10Con(90);
+              break;
+          }
+         }
+         else if(firstrowup.includes(ending))
+         {
+          switch(ending)
+          {
+            case 17:
+              path10Con(-90);path9Con(165);
+              break;
+            case 18:
+              path10Con(-90);path9Con(180);path17Con(-90);
+              break;
+            case 19:
+              path10Con(-90);path9Con(180);path17Con(-45);
+              break;
+            case 20:
+              path10Con(-90);path9Con(180);path17Con(0);
+              break;
+          }
+
+         }
+         else
+         {
+          removeAlll();
+          alert("The destination is on the same row.");
+         }
+
   } else if (firstrowdownpara.includes(starting)) {
+      
   } else alert("Location is on the same row.");
+
 };
